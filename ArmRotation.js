e934f957b2bdf9a1190935ef8cf80d974252c3a8
TheGuyWhoCodes <christopherariagno@gmail.com>
2015-02-14 15:42:04 -0600

    more scripts

---
+++ ArmRotation.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+ 
+var rotationOffset : int = 0;
+ 
+function Update ()
+{
+	
+    var object_pos : Vector3 = transform.position;
+   
+    var mouse_pos : Vector3 = Input.mousePosition;
+   
+    object_pos = Camera.main.WorldToScreenPoint(transform.position);
+   
+    mouse_pos.x = mouse_pos.x - object_pos.x;
+    mouse_pos.y = mouse_pos.y - object_pos.y;
+   
+    var rotZ = Mathf.Atan2(mouse_pos.y, mouse_pos.x) *    Mathf.Rad2Deg;
+   
+    transform.rotation = Quaternion.Euler(Vector3(00, 0, rotZ + rotationOffset));
+}
---
+++ Splash to Menu.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+
+function Start () {
+	yield WaitForSeconds(2.5);
+	Application.LoadLevel ("World 1");
+}
+
+function Update () {
+
+}
